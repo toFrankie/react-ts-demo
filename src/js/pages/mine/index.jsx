f98dd3b684664c5503d7fc3716564994760d9ddc
Frankie <1426203851@qq.com>
2021-07-07 23:47:49 +0800

    refactor: 调整路由

---
+++ src/js/pages/mine/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Mine() {
+  return <div>This is Mine Page.</div>
+}
--- src/root.jsx
+++ src/root.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
-import { Sub1, Sub2 } from './js/components'
+import { HashRouter, Route, Switch, Redirect } from 'react-router-dom'
+import { Home, Mine } from './js/pages'
 
 export default function Root() {
   return (
-    <div>
-      <Sub1 name="Frankie" />
-      <Sub2 name="Mandy" />
-    </div>
+    <HashRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/mine" component={Mine} />
+        <Redirect to="/" />
+      </Switch>
+    </HashRouter>
   )
 }
+
+// import React from 'react'
+// import { hashHistory, Router, Route } from 'react-router'
+// import { Home, Mine } from './js/pages'
+
+// export default function Root() {
+//   return (
+//     <Router history={hashHistory}>
+//       <Route path="/" component={Home} />
+//       <Route path="/mine" component={Mine} />
+//     </Router>
+//   )
+// }
