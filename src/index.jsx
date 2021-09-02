3f2d9f989e979b78ed75a57cb3a524bed73eb301
Frankie <1426203851@qq.com>
2021-09-03 04:11:25 +0800

    refactor: 修改根节点

--- src/index.jsx
+++ src/index.jsx
@@ -2,5 +2,5 @@ import React from 'react'
 import { render } from 'react-dom'
 import Root from './root'
 
-const rootEl = document.getElementById('root')
+const rootEl = document.getElementById('app-root')
 render(<Root />, rootEl)
