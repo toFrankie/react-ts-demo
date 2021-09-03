37676c859b0f440b2dffc82358f1e69a6606983f
Frankie <1426203851@qq.com>
2021-09-03 23:04:44 +0800

    build: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,15 @@
 module.exports = {
   root: true,
   parser: '@babel/eslint-parser',
-  extends: ['alloy', 'alloy/react'],
+  extends: ['alloy', 'alloy/react', 'plugin:react-hooks/recommended'],
+  plugins: ['react-hooks'],
   settings: {
     react: {
       version: 'detect'
     }
+  },
+  rules: {
+    'react-hooks/rules-of-hooks': 2,
+    'react-hooks/exhaustive-deps': 1
   }
 }
